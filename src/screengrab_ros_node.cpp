ca56b68f07d633abe33c2b7b8618e88c86f7ec0c
Lucas Walter <wsacul@gmail.com>
2013-11-13 11:14:15 -0800

    Not using the mask bits for now, just delete them.

--- src/screengrab_ros_node.cpp
+++ src/screengrab_ros_node.cpp
@@ -14,79 +14,19 @@ void XImage2RosImage(XImage& ximage, Display& _xDisplay, Screen& _xScreen,
     im->header.stamp = ros::Time::now();
 
     if (_xScreen.depths->depth == 24) {
-        // Some of the following code is borrowed from http://www.roard.com/docs/cookbook/cbsu19.html ("Screen grab with X11" - by Marko Riedel, with an idea by Alexander Malmberg)
-        unsigned long rmask = _xScreen.root_visual->red_mask,
-                gmask = _xScreen.root_visual->green_mask,
-                bmask = _xScreen.root_visual->blue_mask;
-        unsigned long rshift, rbits, gshift, gbits, bshift, bbits;
-        unsigned char colorChannel[3];
-
-        rshift = 0;
-        rbits = 0;
-        while (!(rmask & 1)) {
-            rshift++;
-            rmask >>= 1;
-        }
-        while (rmask & 1) {
-            rbits++;
-            rmask >>= 1;
-        }
-        if (rbits > 8) {
-            rshift += rbits - 8;
-            rbits = 8;
-        }
-
-        gshift = 0;
-        gbits = 0;
-        while (!(gmask & 1)) {
-            gshift++;
-            gmask >>= 1;
-        }
-        while (gmask & 1) {
-            gbits++;
-            gmask >>= 1;
-        }
-        if (gbits > 8) {
-            gshift += gbits - 8;
-            gbits = 8;
-        }
-
-        bshift = 0;
-        bbits = 0;
-        while (!(bmask & 1)) {
-            bshift++;
-            bmask >>= 1;
-        }
-        while (bmask & 1) {
-            bbits++;
-            bmask >>= 1;
-        }
-        if (bbits > 8) {
-            bshift += bbits - 8;
-            bbits = 8;
-        }
-
+       // the code just deleted here is probably more robust than
+       // a straight memcpy, but for now go without it.
        const int wd = ximage.width;
        const int ht = ximage.height;
        const int frame_size = wd * ht * 4;
        im->width = wd;
        im->height = ht;
        im->step = im->width * 4;
+       // maybe this could be extracted from X
        im->encoding = sensor_msgs::image_encodings::BGRA8;
        im->data.resize(frame_size);
        memcpy(&im->data[0], ximage.data, frame_size); 
-       #if 0
-        for (unsigned int x = 0; x < ximage.width; x++) {
-            for (unsigned int y = 0; y < ximage.height; y++) {
-                color.pixel = XGetPixel(&ximage, x, y);
-                colorChannel[0] = ((color.pixel >> bshift) & ((1 << bbits) - 1)) << (8 - bbits);
-                colorChannel[1] = ((color.pixel >> gshift) & ((1 << gbits) - 1)) << (8 - gbits);
-                colorChannel[2] = ((color.pixel >> rshift) & ((1 << rbits) - 1)) << (8 - rbits);
-                cv::Vec4b col = cv::Vec4b(colorChannel[0], colorChannel[1], colorChannel[0],0);
-                tmp.at<cv::Vec4b> (y,x) = col;
-            }
-        }
-        #endif
+
     } else { // Extremly slow alternative for non 24bit-depth
         Colormap colmap = DefaultColormap(&_xDisplay, DefaultScreen(&_xDisplay));
         for (unsigned int x = 0; x < ximage.width; x++) {
